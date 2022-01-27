ae0e37c1eeb3c676b3dcee9fd10a40eaa3be0cfe
serii <seriiburduja@gmail.com>
2022-01-27 18:04:49 +0200

    feat: css and js works

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,29 @@
 const mix = require('laravel-mix');
+let productionSourceMaps = false;
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.setPublicPath('public/build')
+    .setResourceRoot('build')
+    .js('resources/js/app.js', 'js')
+    .sass('resources/sass/app.scss', 'css')
+    .version()
+    .options({
+        processCssUrls: false
+    })
+    .sourceMaps(productionSourceMaps, 'source-map')
+    .browserSync({
+        proxy: 'https://localhost:8088/',
+        files: [
+            'public/build/css/app.css',  // Generated .css file
+            'public/build/js/app.js',    // Generated .js file
+            'public/**/*.+(html|php)',          // Generic .html and/or .php files [no specific platform]
+            'app/**/*.+(html|php)',          // Generic .html and/or .php files [no specific platform]
+            'resources/views/**/*.php', // Laravel-specific view files
+        ],
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0'
+            }
+        },
+        open: false
+    });
