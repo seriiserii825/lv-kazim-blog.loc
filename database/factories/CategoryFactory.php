913c30562abf21ea7a541a072768c10beb812a48
serii <seriiburduja@gmail.com>
2022-01-31 12:46:35 +0200

    feat: seadded category

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class PostFactory extends Factory
+class CategoryFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -14,9 +14,7 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            'title' => $this->faker->name,
-            'post_text' => $this->faker->text,
-            'category_id' => random_int(1, 5),
+            'title' => $this->faker->word
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Category::factory(5)->create();
         Post::factory(10)->create();
     }
 }
